1dfcf6844b1f5b2599e0d77ea9e040d27f35aea8
michelleblom <michelle.blom@gmail.com>
2020-04-13 16:50:26 +1000

    Edited column names in ballot manifest style to match next pilot.

--- Code/dominion_tools.py
+++ Code/dominion_tools.py
@@ -15,6 +15,8 @@ def prep_dominion_manifest(manifest, N_cards, n_cvrs):
     Prepare a Dominion Excel ballot manifest (read as a pandas dataframe) for sampling.
     The manifest may have cards that do not contain the contest, but every listed CVR
     is assumed to have a corresponding card in the manifest.
+
+    NOTE: This function has been edited to cater for special primaries manifest style.
     
     If the number of CVRs n_cvrs is less than the number of cards that might contain the contest,
     N_cards, an additional "phantom" batch is added to the manifest to make up the difference.
@@ -33,16 +35,16 @@ def prep_dominion_manifest(manifest, N_cards, n_cvrs):
     manifest_cards : the total number of cards in the manifest
     phantom_cards : the number of phantom cards required
     """
-    cols = ['Tray #', 'Tabulator Number', 'Batch Number', 'Total Ballots', 'VBMCart.Cart number']
+    cols = ['State', 'Tabulator ID', 'Batch ID', 'Ballot Count', 'Transfer Case #']
     assert set(cols).issubset(manifest.columns), "missing columns"
-    manifest_cards = manifest['Total Ballots'].sum()
+    manifest_cards = manifest['Ballot Count'].sum()
     if n_cvrs < N_cards:
         warnings.warn('The CVR list does not account for every card cast in the contest; adding a phantom batch to the manifest')
-        r = {'Tray #': None, 'Tabulator Number': 'phantom', 'Batch Number': 1, \
-             'Total Ballots': N_cards-n_cvrs, 'VBMCart.Cart number': None}
+        r = {'State': None, 'Tabulator ID': 'phantom', 'Batch ID': 1, \
+             'Ballot Count': N_cards-n_cvrs, 'Transfer Case #': None}
         manifest = manifest.append(r, ignore_index = True)
-    manifest['cum_cards'] = manifest['Total Ballots'].cumsum()    
-    for c in ['Tray #', 'Tabulator Number', 'Batch Number', 'VBMCart.Cart number']:
+    manifest['cum_cards'] = manifest['Ballot Count'].cumsum()    
+    for c in ['State', 'Tabulator ID', 'Batch ID', 'Transfer Case #']:
         manifest[c] = manifest[c].astype(str)
     return manifest, manifest_cards, N_cards - n_cvrs
 
@@ -105,9 +107,9 @@ def sample_from_manifest(manifest, sample):
     for s in sample:
         batch_num = int(np.searchsorted(lookup, s, side='left'))
         card_in_batch = int(s-lookup[batch_num-1])
-        tab = manifest.iloc[batch_num-1]['Tabulator Number']
-        batch = manifest.iloc[batch_num-1]['Batch Number']
-        card = list(manifest.iloc[batch_num-1][['VBMCart.Cart number','Tray #']]) \
+        tab = manifest.iloc[batch_num-1]['Tabulator ID']
+        batch = manifest.iloc[batch_num-1]['Batch ID']
+        card = list(manifest.iloc[batch_num-1][['Transfer Case #','State']]) \
                 + [tab, batch, card_in_batch, str(tab)+'-'+str(batch)\
                 + '-'+str(card_in_batch), s]
         cards.append(card)
@@ -141,10 +143,10 @@ def sample_from_cvr(cvr_list, manifest, sample):
         cvr_id = cvr_list[s].id
         tab, batch, card_num = cvr_id.split("-")
         if not cvr_list[s].phantom:
-            manifest_row = manifest[(manifest['Tabulator Number'] == str(tab)) \
-                                    & (manifest['Batch Number'] == str(batch))].iloc[0]
-            card = [manifest_row['VBMCart.Cart number'],\
-                    manifest_row['Tray #']] \
+            manifest_row = manifest[(manifest['Tabulator ID'] == str(tab)) \
+                                    & (manifest['Batch ID'] == str(batch))].iloc[0]
+            card = [manifest_row['Transfer Case #'],\
+                    manifest_row['State']] \
                     + [tab, batch, card_num, str(tab)+'-'+str(batch)\
                     + '-'+str(card_num), s]
         else:
@@ -167,7 +169,7 @@ def write_cards_sampled(sample_file, cards, print_phantoms=True):
         filename for output
         
     cards : list of lists
-        'VBMCart.Cart number','Tray #','Tabulator Number','Batch Number', 'ballot_in_batch', 
+        'Transfer Case #','State','Tabulator ID','Batch ID', 'ballot_in_batch', 
               'imprint', 'absolute_card_index'
     
     print_phantoms : Boolean
@@ -196,20 +198,20 @@ def test_sample_from_manifest():
     Test the card lookup function
     """
     sample = [1, 99, 100, 101, 121, 200, 201]
-    d = [{'Tray #': 1, 'Tabulator Number': 17, 'Batch Number': 1, 'Total Ballots': 100, 'VBMCart.Cart number': 1},\
-        {'Tray #': 2, 'Tabulator Number': 18, 'Batch Number': 2, 'Total Ballots': 100, 'VBMCart.Cart number': 2},\
-        {'Tray #': 3, 'Tabulator Number': 19, 'Batch Number': 3, 'Total Ballots': 100, 'VBMCart.Cart number': 3}]
+    d = [{'State': 'Kansas', 'Tabulator ID': 17, 'Batch ID': 1, 'Ballot Count': 100, 'Transfer Case #': 'TC-001'},\
+        {'State': 'Kansas', 'Tabulator ID': 18, 'Batch ID': 2, 'Ballot Count': 100, 'Transfer Case #': 'TC-002'},\
+        {'State': 'Kansas', 'Tabulator ID': 19, 'Batch ID': 3, 'Ballot Count': 100, 'Transfer Case #': 'TC-003'}]
     manifest = pd.DataFrame.from_dict(d)
-    manifest['cum_cards'] = manifest['Total Ballots'].cumsum()
+    manifest['cum_cards'] = manifest['Ballot Count'].cumsum()
     cards = sample_from_manifest(manifest, sample)
     # cart, tray, tabulator, batch, card in batch, imprint, absolute index
-    assert cards[0] == [1, 1, 17, 1, 1, "17-1-1",1]
-    assert cards[1] == [1, 1, 17, 1, 99, "17-1-99",99]
-    assert cards[2] == [1, 1, 17, 1, 100, "17-1-100",100]
-    assert cards[3] == [2, 2, 18, 2, 1, "18-2-1",101]
-    assert cards[4] == [2, 2, 18, 2, 21, "18-2-21",121]
-    assert cards[5] == [2, 2, 18, 2, 100, "18-2-100",200]
-    assert cards[6] == [3, 3, 19, 3, 1, "19-3-1",201]  
+    assert cards[0] == ['TC-001', 'Kansas', 17, 1, 1, "17-1-1",1]
+    assert cards[1] == ['TC-001', 'Kansas', 17, 1, 99, "17-1-99",99]
+    assert cards[2] == ['TC-001', 'Kansas', 17, 1, 100, "17-1-100",100]
+    assert cards[3] == ['TC-002', 'Kansas', 18, 2, 1, "18-2-1",101]
+    assert cards[4] == ['TC-002', 'Kansas', 18, 2, 21, "18-2-21",121]
+    assert cards[5] == ['TC-002', 'Kansas', 18, 2, 100, "18-2-100",200]
+    assert cards[6] == ['TC-003', 'Kansas', 19, 3, 1, "19-3-1",201]  
 
 if __name__ == "__main__":
     test_sample_from_manifest()
