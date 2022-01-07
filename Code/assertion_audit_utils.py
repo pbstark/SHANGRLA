434aded7dd9f6e8588d59f4c8a176e975eb90a88
PHILIP B. STARK <pbstark@berkeley.edu>
2022-01-07 13:28:53 -0800

    ENH: added ALPHA martingale

--- Code/assertion_audit_utils.py
+++ Code/assertion_audit_utils.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import scipy as sp
+from scipy.stats import bernoulli
 import pandas as pd
 import json
 import csv
@@ -930,7 +931,7 @@ class TestNonnegMean:
         for a population of size N of values in the interval [0, u].
     
         Generalizes Wald's SPRT for the Bernoulli to sampling without replacement and to 
-        nonnegative bounded values rather than binary values. 
+        nonnegative bounded values rather than binary values (for which it is a valid test, but not the SPRT). 
         See Stark, 2022. ALPHA: Audit that Learns from Previous Hand-Audited Ballots
 
         If N is finite, assumes the sample is drawn without replacement
@@ -967,6 +968,79 @@ class TestNonnegMean:
             terms = np.cumprod((x*eta/m + (u-x)*(u-eta)/(u-m))/u) # generalization of Bernoulli SPRT
         terms[m<0] = np.inf                        # the null is surely false
         return 1/np.max(np.cumprod(terms)) if random_order else 1/np.prod(terms)
+
+    @classmethod
+    def shrink_trunc(cls, x: np.array, N: int, mu: float=1/2, nu: float=1-np.finfo(float).eps, u: float=1, c: float=1/2, 
+                 d: float=100) -> np.array: 
+        '''
+        apply the shrinkage and truncation estimator to an array
+
+        sample mean is shrunk towards nu, with relative weight d compared to a single observation.
+        estimate is truncated above at u-u*eps and below at mu_j+e_j(c,j)
+
+        S_1 = 0
+        S_j = \sum_{i=1}^{j-1} x_i, j > 1
+        m_j = (N*mu-S_j)/(N-j+1) if np.isfinite(N) else mu
+        e_j = c/sqrt(d+j-1)
+        eta_j =  ( (d*nu + S_j)/(d+j-1) \vee (m_j+e_j) ) \wedge u*(1-eps)
+
+        Parameters
+        ----------
+        x : np.array
+            input data       
+        mu : float in (0, 1)
+            hypothesized population mean
+        eta : float in (t, 1)
+            initial alternative hypothethesized value for the population mean
+        c : positive float
+            scale factor for allowing the estimated mean to approach t from above
+        d : positive float
+            relative weight of nu compared to an observation, in updating the alternative for each term
+        '''
+        S = np.insert(np.cumsum(x),0,0)[0:-1]  # 0, x_1, x_1+x_2, ...,  
+        j = np.arange(1,len(x)+1)              # 1, 2, 3, ..., len(x)
+        m = (N*mu-S)/(N-j+1) if np.isfinite(N) else mu   # mean of population after (j-1)st draw, if null is true 
+        return np.minimum(u*(1-np.finfo(float).eps), np.maximum((d*nu+S)/(d+j-1),m+c/np.sqrt(d+j-1)))
+    
+    @classmethod
+    def alpha_mart(cls, x: np.array, N: int, mu: float=1/2, eta: float=1-np.finfo(float).eps, u: float=1, \
+                   estim: callable=shrink_trunc) -> float :
+        '''
+        Finds the ALPHA martingale for the hypothesis that the population 
+        mean is less than or equal to t using a martingale method,
+        for a population of size N, based on a series of draws x.
+
+        The draws must be in random order, or the sequence is not a martingale under the null
+
+        If N is finite, assumes the sample is drawn without replacement
+        If N is infinite, assumes the sample is with replacement
+
+        Parameters
+        ----------
+        x : list corresponding to the data
+        N : int
+            population size for sampling without replacement, or np.infinity for sampling with replacement
+        mu : float in (0,1)
+            hypothesized fraction of ones in the population
+        eta : float in (t,1) 
+            alternative hypothesized population mean
+        estim : callable
+            estim(x, N, mu, eta, u) -> np.array of length len(x), the sequence of values of eta_j for ALPHA
+
+        Returns
+        -------   
+        terms : array
+            sequence of terms that would be a nonnegative martingale under the null
+        '''
+        S = np.insert(np.cumsum(x),0,0)[0:-1]  # 0, x_1, x_1+x_2, ...,  
+        j = np.arange(1,len(x)+1)              # 1, 2, 3, ..., len(x)
+        m = (N*mu-S)/(N-j+1) if np.isfinite(N) else mu   # mean of population after (j-1)st draw, if null is true 
+        etaj = estim(x, N, mu, eta, u) 
+        with np.errstate(divide='ignore',invalid='ignore'):
+            terms = np.cumprod((x*etaj/m + (1-x)*(u-etaj)/(u-m))/u)
+        terms[m<0] = np.inf
+        return np.min([1, 1/np.max(terms)])
+
 
     @classmethod
     def kaplan_markov(cls, x, t=1/2, g=0, random_order=True):
@@ -2150,6 +2224,36 @@ def test_assign_sample_nums():
     assert cvrs[0].get_sample_num() == 100208482908198438057700745423243738999845662853049614266130533283921761365671
     assert cvrs[5].sample_num == 93838330019164869717966768063938259297046489853954854934402443181124696542865
     
+def test_shrink_trunc():
+    epsj = lambda c, d, j: c/math.sqrt(d+j-1)
+    Sj = lambda x, j: 0 if j==1 else np.sum(x[0:j-1])
+    muj = lambda N, mu, x, j: (N*mu - Sj(x, j))/(N-j+1) if np.isfinite(N) else mu
+    nus = [.51, .55, .6]
+    mu = 1/2
+    u = 1
+    d = 10
+    vrand =  sp.stats.bernoulli.rvs(1/2, size=20)
+    v = [
+        np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1]),
+        np.array([1, 1, 1, 1, 1, 1, 0, 0, 0, 0]),
+        vrand
+    ]
+    for nu in nus:
+        c = (nu-mu)/2
+        for x in v:
+            N = len(x)
+            xinf = TestNonnegMean.shrink_trunc(x, np.inf, mu, nu, c=c, d=d)
+            xfin = TestNonnegMean.shrink_trunc(x, len(x), mu, nu, c=c, d=d)
+            yinf = np.zeros(len(x))
+            yfin = np.zeros(len(x))
+            for j in range(1,len(x)+1):
+                est = (d*nu + Sj(x,j))/(d+j-1)
+                most = u*(1-np.finfo(float).eps)
+                yinf[j-1] = np.minimum(np.maximum(mu+epsj(c,d,j), est), most)
+                yfin[j-1] = np.minimum(np.maximum(muj(N,mu,x,j)+epsj(c,d,j), est), most)
+            np.testing.assert_allclose(xinf, yinf)    
+            np.testing.assert_allclose(xfin, yfin)    
+    
 if __name__ == "__main__":
     test_cvr_from_raire()
     test_cvr_from_dict()
@@ -2173,3 +2277,5 @@ if __name__ == "__main__":
     test_kaplan_kolmogorov()
     test_initial_sample_size()
     test_initial_sample_size_KW()
+    
+    test_shrink_trunc()
