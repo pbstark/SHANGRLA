76a3d3467051cac05cef67a5280982cdb6f09fa4
bsheehan-SF-RLA <165421800+bsheehan-SF-RLA@users.noreply.github.com>
2024-04-01 17:09:53 -0700

    Support newer Dominion CVR format, support selecting adjudicated CVRs, select by group (e.g. VBM only); add Dominion.read_cvrs_directory() convenience class method; added tests for all new features (#79)

--- shangrla/shangrla/tests/test_Dominion.py
+++ shangrla/shangrla/tests/test_Dominion.py
@@ -24,6 +24,136 @@ from shangrla.Hart import Hart
 ##########################################################################################
 
 class TestDominion:
+    def test_read_cvrs_old_format(self):
+        """
+        Test reading the older Dominion CVR format
+        """
+        cvr_list = Dominion.read_cvrs(
+            "shangrla/shangrla/tests/Data/Dominion_CVRs/test_5.2.18.2.Dominion.json"
+        )
+        assert len(cvr_list) == 2
+        cvr_1, cvr_2 = cvr_list
+        assert cvr_1.id == "60001_1_1"
+        assert list(cvr_1.votes.keys()) == ["111"]
+        assert cvr_1.votes["111"] == {"6": 1, "1": 2}
+        assert cvr_1.get_vote_for("111", "6")
+        assert cvr_1.get_vote_for("111", "1")
+        assert cvr_1.get_vote_for("111", "999") is False
+        assert cvr_2.id == "60009_3_21"
+        assert list(cvr_2.votes.keys()) == ["111", "122"]
+        assert cvr_2.votes["111"] == {"6": 1}
+        assert cvr_2.votes["122"] == {"9": 1, "48": 2}
+        assert cvr_2.get_vote_for("111", "6")
+        assert cvr_2.get_vote_for("111", "1") is False
+        assert cvr_2.get_vote_for("122", "9")
+        assert cvr_2.get_vote_for("122", "48")
+        assert cvr_2.get_vote_for("122", "999") is False
+
+    def test_read_cvrs_old_format_adjudicated(self):
+        """
+        Tests using the "Modified" (adjudicated) data from the CVR (if present) over the "Original"
+        Ingests the older Dominion CVR format
+        """
+        cvr_list = Dominion.read_cvrs(
+            "shangrla/shangrla/tests/Data/Dominion_CVRs/test_5.2.18.2.Dominion.json",
+            use_adjudicated=True,
+        )
+        assert len(cvr_list) == 2
+        cvr_1, cvr_2 = cvr_list
+        assert cvr_1.id == "60001_1_1"
+        assert list(cvr_1.votes.keys()) == ["111"]
+        assert cvr_1.votes["111"] == {"6": 1, "1": 2}
+        assert cvr_1.get_vote_for("111", "6")
+        assert cvr_1.get_vote_for("111", "1")
+        assert cvr_1.get_vote_for("111", "999") is False
+        assert cvr_2.id == "60009_3_21"
+        assert list(cvr_2.votes.keys()) == ["111", "122"]
+        assert cvr_2.votes["111"] == {"6": 1}
+        assert cvr_2.votes["122"] == {"9": 1}
+        assert cvr_2.get_vote_for("111", "6")
+        assert cvr_2.get_vote_for("111", "1") is False
+        assert cvr_2.get_vote_for("122", "9")
+        assert cvr_2.get_vote_for("122", "48") is False
+        assert cvr_2.get_vote_for("122", "999") is False
+
+    def test_read_cvrs_new_format(self):
+        """
+        Test reading the newer Dominion CVR format
+        """
+        cvr_list = Dominion.read_cvrs(
+            "shangrla/shangrla/tests/Data/Dominion_CVRs/test_5.10.50.85.Dominion.json"
+        )
+        assert len(cvr_list) == 2
+        cvr_1, cvr_2 = cvr_list
+        assert cvr_1.id == "1_2_13"
+        assert list(cvr_1.votes.keys()) == ["1"]
+        assert cvr_1.votes["1"] == {"5": 1}
+        assert cvr_1.get_vote_for("1", "5")
+        assert cvr_1.get_vote_for("1", "999") is False
+        assert cvr_2.id == "1_5_119"
+        assert list(cvr_2.votes.keys()) == ["1"]
+        assert cvr_2.votes["1"] == {"6": 1}
+        assert cvr_2.get_vote_for("1", "6")
+        assert cvr_2.get_vote_for("1", "999") is False
+
+    def test_read_cvrs_new_format_adjudicated(self):
+        """
+        Tests using the "Modified" (adjudicated) data from the CVR (if present) over the "Original"
+        Ingests the newer Dominion CVR format
+        """
+        cvr_list = Dominion.read_cvrs(
+            "shangrla/shangrla/tests/Data/Dominion_CVRs/test_5.10.50.85.Dominion.json",
+            use_adjudicated=True,
+        )
+        assert len(cvr_list) == 2
+        cvr_1, cvr_2 = cvr_list
+        assert cvr_1.id == "1_2_13"
+        assert list(cvr_1.votes.keys()) == ["1"]
+        assert cvr_1.votes["1"] == {"5": 1}
+        assert cvr_1.get_vote_for("1", "5")
+        assert cvr_1.get_vote_for("1", "999") is False
+        assert cvr_2.id == "1_5_119"
+        assert list(cvr_2.votes.keys()) == ["1"]
+        assert cvr_2.votes["1"] == {}
+        assert cvr_2.get_vote_for("1", "6") is False
+        assert cvr_2.get_vote_for("1", "999") is False
+
+    def test_read_cvrs_new_format_vbm_only(self):
+        """
+        Test reading the newer Dominion CVR format, selecting VBM results only
+        """
+        cvr_list = Dominion.read_cvrs(
+            "shangrla/shangrla/tests/Data/Dominion_CVRs/test_5.10.50.85.Dominion.json",
+            include_groups=(2,),
+        )
+        assert len(cvr_list) == 1
+        cvr_2 = cvr_list[0]
+        assert cvr_2.id == "1_5_119"
+        assert list(cvr_2.votes.keys()) == ["1"]
+        assert cvr_2.votes["1"] == {"6": 1}
+        assert cvr_2.get_vote_for("1", "6")
+        assert cvr_2.get_vote_for("1", "999") is False
+
+    def test_read_cvrs_directory(self):
+        """
+        Tests the convenience function for reading all CVRs from a given directory
+        """
+        cvr_list = Dominion.read_cvrs_directory(
+            "shangrla/shangrla/tests/Data/Dominion_CVRs/CVR_Export",
+        )
+        assert len(cvr_list) == 2
+        cvr_1, cvr_2 = cvr_list
+        assert cvr_1.id == "1_2_13"
+        assert list(cvr_1.votes.keys()) == ["1"]
+        assert cvr_1.votes["1"] == {"5": 1}
+        assert cvr_1.get_vote_for("1", "5")
+        assert cvr_1.get_vote_for("1", "999") is False
+        assert cvr_2.id == "1_5_119"
+        assert list(cvr_2.votes.keys()) == ["1"]
+        assert cvr_2.votes["1"] == {"6": 1}
+        assert cvr_2.get_vote_for("1", "6")
+        assert cvr_2.get_vote_for("1", "999") is False
+
     def test_sample_from_manifest(self):
         """
         Test the card lookup function
