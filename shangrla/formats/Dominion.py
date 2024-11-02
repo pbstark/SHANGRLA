bb8b8a38633988a92a975dc6d5373b85d89567d0
Philip B. Stark <pbstark@berkeley.edu>
2024-11-02 16:47:42 -0700

    Oneaudit (#92)
    * WIP: improving Dominion support, progress towards ONEAudit integration
    
    * WIP: Dominion tools
    
    * Dominion "multiple card" fix, Testcase Regression Fixes (#91)
    
    * This commit fixes issues with the record_id not always set, and the use_current flag never being used
    
    * Fix earlier issue where only the first 'Card' in the Dominion output was parsed, added associated unit tests
    
    ---------
    
    Co-authored-by: Philip B. Stark <pbstark@berkeley.edu>
    
    * WIP: ONEAudit integration. Dominion mvr retrieval improvements
    
    * WIP: Dominion card numbering, construct pool_means only for pool==True CVRs
    
    * WIP: working but sample size estimation needs to be improved
    
    * ENH: ONEAudit seems to be working, including sample size estimation.
    
    * ENH: improve documentation in ONEAudit notebook; minor bug fixes
    
    ---------
    
    Co-authored-by: bsheehan-SF-RLA <165421800+bsheehan-SF-RLA@users.noreply.github.com>

--- shangrla/formats/Dominion.py
+++ shangrla/formats/Dominion.py
@@ -9,7 +9,9 @@ import warnings
 import re
 import glob
 from collections.abc import Collection
-from shangrla.core.Audit import CVR
+from collections import defaultdict
+from shangrla.core.Audit import Audit, CVR
+from shangrla.core.NonnegMean import NonnegMean
 
 
 class Dominion:
@@ -83,7 +85,8 @@ class Dominion:
     def read_cvrs(
         cls,
         cvr_file: str,
-        use_adjudicated: bool = False,
+        use_current: bool = True,
+        enforce_rules: bool = True,
         include_groups: Collection = [],
         pool_groups: Collection = [],
     ):
@@ -98,20 +101,27 @@ class Dominion:
         -----------
         cvr_file: string
             filename for cvrs
-        use_adjudicated: bool [optional], default False
-            if set, read marks from ["Modified"] instead of ["Original"]
+        use_current: bool [optional], default True
+            if set, ignores votes unless `IsCurrent == True`
+        enforce_rules: bool [optional], default True
+            if set, ignores votes unless `IsVote == True`
         include_groups: enumerable
             if nonempty, use to select only CVRs with specified "CountingGroupId", e.g. (2,) for VBM
         pool_groups: enumerable
-            if nonempty, CVRs with "CountingGroupId" in any of the groups is labeled as pooled (for ONEAudit)
+            if nonempty, CVRs with `CountingGroupId` in any of the groups is labeled as pooled (for ONEAudit)
+            for subsequent construction of ONEAudit CVRs based on aggregating within each `tally_pool` with
+            that `CountingGroupId`.
 
         Returns:
         --------
-        cvr_list: list of CVR objects
+        cvr_list: list of CVR objects, with additional fields, viz,
+                  `id=str(c["TabulatorId"]) + "_" + str(c["BatchId"])  + "_"  + str(record_id)`
+                  `tally_pool=str(c["TabulatorId"]) + "_" + str(c["BatchId"])`
+                  `pool=(c["CountingGroupId"] in pool_groups)`
 
         """
         # Image mask is used if the RecordId has been obfuscated (see below)
-        image_mask_pattern = re.compile(r"[0-9]{5}_[0-9]{5}_[0-9]{6}")
+        image_mask_pattern = re.compile(r"[0-9]{5}_[0-9]{5}_[0-9]*")
 
         with open(cvr_file, "r") as f:
             cvr_json = json.load(f)
@@ -124,29 +134,36 @@ class Dominion:
             # Skip CVRs not in the desired include_group (if set)
             if include_groups and c["CountingGroupId"] not in include_groups:
                 continue
-            # Use adjudicated CVR data (if present and requested)
-            _ckey = (
-                "Modified"
-                if ("Modified" in c.keys() and use_adjudicated)
-                else "Original"
-            )
-            # Dominion somewhere between 5.2.18.2 and 5.10.50.85 added another hierarchical level, "Cards"
-            if "Cards" in c[_ckey].keys():
-                _selector = c[_ckey]["Cards"][0]["Contests"]
-            else:
-                _selector = c[_ckey]["Contests"]
-            for con in _selector:
-                contest_votes = {}
-                for mark in con["Marks"]:
-                    contest_votes[str(mark["CandidateId"])] = mark["Rank"]
-                votes[str(con["Id"])] = contest_votes
+            # Use adjudicated/updated CVR data (if present and requested)
+            for k in [
+                j
+                for j in c.keys()
+                if j in (["Original", "Modified"] if use_current else ["Original"])
+            ]:
+                # Dominion somewhere between 5.2.18.2 and 5.10.50.85 added another hierarchical level, "Cards"
+                if "Cards" in c[k].keys():
+                    # List comprehension to combine a list of lists of contests, which is essentially
+                    # the contents of c[k]["Cards"][0:n]["Contests"], where n is the number of "Card" entries
+                    _selector = [
+                        _con
+                        for _eachlist in [_c["Contests"] for _c in c[k]["Cards"]]
+                        for _con in _eachlist
+                    ]
+                else:
+                    _selector = c[k]["Contests"]
+                for con in _selector:
+                    contest_votes = {}
+                    for mark in con["Marks"]:
+                        contest_votes[str(mark["CandidateId"])] = (
+                            mark["Rank"] if (mark["IsVote"] or not enforce_rules) else 0
+                        )
+                    votes[str(con["Id"])] = contest_votes
             # If RecordId is obfuscated, extract it from the ImageMask
-            if c["RecordId"] == "X":
+            record_id = c["RecordId"]
+            if record_id == "X":
                 image_match = image_mask_pattern.search(c["ImageMask"])
                 if image_match is not None:
                     record_id = int(image_match.group(0).split("_")[-1])
-            else:
-                record_id = c["RecordId"]
             cvr_list.append(
                 CVR(
                     id=str(c["TabulatorId"])
@@ -165,7 +182,8 @@ class Dominion:
     def read_cvrs_directory(
         cls,
         cvr_directory: str,
-        use_adjudicated: bool = False,
+        use_current: bool = True,
+        enforce_rules: bool = True,
         include_groups: Collection = [],
         pool_groups: Collection = [],
     ):
@@ -176,10 +194,14 @@ class Dominion:
         -----------
         cvr_directory: string
             directory name in which the cvrs can be found
-        use_adjudicated: bool [optional], default False
-            if set, read marks from ["Modified"] instead of ["Original"] (see read_cvrs)
-        include_groups: tuple of ints [optional], default ()
+        use_current: bool [optional], default True
+            if set, ignores votes unless `IsCurrent == True`
+        enforce_rules: bool [optional], default True
+            if set, ignores votes unless `IsVote == True`
+        include_groups: collection of ints [optional], default []
             if set, use to select only CVRs with specified "CountingGroupId" (see read_cvrs)
+        pool_groups: collection of ints [optional], default []
+            if set, mark CVRs for pooling with ONEAudit
 
         Returns:
         --------
@@ -189,7 +211,9 @@ class Dominion:
         cvr_list = []
         for file in [f for f in sorted(glob.glob(f"{cvr_directory}/CvrExport_*.json"))]:
             cvr_list.extend(
-                Dominion.read_cvrs(file, use_adjudicated, include_groups, pool_groups)
+                Dominion.read_cvrs(
+                    file, use_current, enforce_rules, include_groups, pool_groups
+                )
             )
         return cvr_list
 
@@ -210,6 +234,178 @@ class Dominion:
         for c in cvr_list:
             c.id = str(c.id).replace("_", "-")
         return cvr_list
+
+    @classmethod
+    def get_contest_data(
+        contest_manifest,
+        candidate_manifest,
+        tallies,
+        exclude_groups=("WriteIn",),
+    ):
+        """Extract Contest and Candidate information from the manifest files and
+        build a dict of contests which for each contest contains the contest name,
+        number of winners, list of candidates (ids and names), list of winners
+        (ids and names), and vote and card tallies, looking something like this:
+
+        {
+            "1": {
+                  "name": "GOVERNER",
+                  "n_winners": 1,
+                  "social_choice_fn": "PLURALITY",
+                  "candidates": [ {"id": "1", "name": "DAVE DAVESON" },
+                                  {"id": "2", "name": "ERIC ERICSON" },
+                  ],
+                  "winners": [ {"id": "2", "ERIC ERICSON"} ],
+                  "votes": ["2": 12345, "1": 3721],
+                  "cards": 17112,
+            },
+            "2": {
+                  "name": "TREASURER",
+                  "n_winners": 1,
+                  "social_choice_fn": "PLURALITY",
+                  "candidates": [ {"id": "3", "name": "GARY GARYSON" },
+                                  {"id": "4", "name": "MIKE MIKESON" },
+                  ],
+                  "winners": [ {"id": "3", "name": "GARY GARYSON" } ],
+                  "votes": ["3": 6112, "4": 5319],
+                  "cards": 11219,
+            }
+        }
+        """
+
+        # Init some counters, dicts, etc.
+        candidate_list = []
+        candidate_dict = {}
+        contest_dict = {}
+
+        # Ingest ContestManifest.json
+        with open(contest_manifest) as fp:
+            contestdata = json.load(fp)
+
+        # Ingest CandidateManifest.json
+        with open(candidate_manifest) as fp:
+            candidatedata = json.load(fp)
+
+        # Build a list of candidate dicts containing candidate id, candidate name and contest id,
+        # along with a dict mapping candidate id to name, making lookup by id easier
+        for tuple in [
+            (str(c["Id"]), c["Description"], str(c["ContestId"]))
+            for c in candidatedata["List"]
+            if c["Type"] not in exclude_groups
+        ]:
+            candidate_list.append(
+                {"id": tuple[0], "name": tuple[1], "contest": tuple[2]}
+            )
+            candidate_dict[tuple[0]] = tuple[1]
+
+        # Now build a list of contests by contest id, each populating the contest name, number of
+        # winners, social choice function, and candidate list to start
+        for tuple in [
+            (str(c["Id"]), c["Description"], c["VoteFor"], c["NumOfRanks"])
+            for c in contestdata["List"]
+        ]:
+            contest_dict[tuple[0]] = {
+                "name": tuple[1],
+                "n_winners": tuple[2],
+                "social_choice_fn": (
+                    Contest.SOCIAL_CHOICE_FUNCTION.PLURALITY
+                    if tuple[3] == 0
+                    else Contest.SOCIAL_CHOICE_FUNCTION.IRV
+                ),
+                "candidates": [
+                    {"id": cn["id"], "name": cn["name"]}
+                    for cn in candidate_list
+                    if cn["contest"] == tuple[0]
+                ],
+            }
+
+        # Now use the provided tallies to determine the winning candidates in each contest,
+        # as well as the number of ballot cards containing each contest
+
+        votes = tallies["votes"]
+        cards = tallies["cards"]
+
+        for c in contest_dict.keys():
+            try:
+                v = votes[c].items()
+                v = dict(sorted(v, key=lambda item: item[1], reverse=True))
+            except KeyError:
+                v = {}
+            contest_dict[c]["winners"] = [
+                {"id": n, "name": candidate_dict[n]}
+                for n in list(v.keys())[: contest_dict[c]["n_winners"]]
+            ]
+            contest_dict[c]["votes"] = v
+            try:
+                contest_dict[c]["cards"] = cards[c]
+            except KeyError:
+                contest_dict[c]["cards"] = 0
+
+        return contest_dict
+
+    @classmethod
+    def make_contest(
+        id,
+        data,
+        risk_limit: float=0.05,
+        assertion_files={},
+        audit_type=Audit.AUDIT_TYPE.CARD_COMPARISON,
+        test=NonnegMean.alpha_mart,
+        estim=NonnegMean.optimal_comparison,
+    ):
+        """Returns a contest dict suitable for SHANGRLA"""
+        return {
+            "name": data["name"],
+            "risk_limit": risk_limit,
+            "cards": data["cards"],
+            "choice_function": data["social_choice_fn"],
+            "n_winners": data["n_winners"],
+            "candidates": [c["id"] for c in data["candidates"]],
+            "winner": [c["id"] for c in data["winners"]],
+            "assertion_file": (
+                assertion_files[id]
+                if id in assertion_files
+                and data["social_choice_fn"] == Contest.SOCIAL_CHOICE_FUNCTION.IRV
+                else None
+            ),
+            "audit_type": audit_type,
+            "test": test,
+            "estim": estim,
+        }
+
+    @classmethod
+    def generate_contest_dict(
+        cvr_list,
+        contest_manifest,
+        candidate_manifest,
+        assertion_files,
+    ):
+        """
+        Given a list of CVRs, a contest manifest and a candidate manifest, and optionally a dict
+        of assertion files (where the key is the contest ID and the value, a filename, is a string),
+        return a dict of contests ready to process with SHANGRLA
+        """
+    
+        # First, get the vote and card tallies from the CVR list.  The returned value is a dict of
+        # dicts with two top level keys "votes" and "cards".
+        tallies = tally_cvrs(cvr_list)
+
+        # Next, pull the candidate/contest data together and use the tallies to determine each
+        # contest winner
+        cdict = get_contest_data(
+            contest_manifest,
+            candidate_manifest,
+            tallies,
+            exclude_groups=(),
+        )
+
+        # Finally, use the consolidated contest/candidate data to generate the contest dicts
+        # ready for SHANGRLA
+        c = {}
+        for k, v in cdict.items():
+            c[k] = make_contest(k, v, risk_limit=0.05, assertion_files=assertion_files)
+
+        return c
 
     @classmethod
     def sample_from_manifest(cls, manifest, sample):
@@ -293,6 +489,7 @@ class Dominion:
         for i, s in enumerate(sample):
             cvr_sample.append(cvr_list[s])
             cvr_id = cvr_list[s].id
+            card_in_batch = cvr_list[s].card_in_batch
             tab, batch, card_num = cvr_id.split("-")
             card_id = f"{tab}-{batch}-{card_num}"
             if not cvr_list[s].phantom:
@@ -303,7 +500,7 @@ class Dominion:
                 card = [manifest_row["VBMCart.Cart number"], manifest_row["Tray #"]] + [
                     tab,
                     batch,
-                    card_num,
+                    card_in_batch,
                     card_id,
                 ]
             else:
@@ -319,7 +516,7 @@ class Dominion:
 
     @classmethod
     def write_cards_sampled(
-        cls, sample_file: str, cards: list, print_phantoms: bool = True
+        cls, sample_file: str, cards: list, print_phantoms: bool=True
     ):
         """
         Write the identifiers of the sampled CVRs to a file.
@@ -331,9 +528,9 @@ class Dominion:
 
         cards: list of lists
             'VBMCart.Cart number','Tray #','Tabulator Number','Batch Number', 'ballot_in_batch',
-                  'imprint', 'absolute_card_index'
+                  'imprint'
 
-        print_phantoms: Boolean
+        print_phantoms: bool
             if print_phantoms, prints all sampled cards, including "phantom" cards that were not in
             the original manifest.
             if not print_phantoms, suppresses "phantom" cards
@@ -351,8 +548,7 @@ class Dominion:
                     "tabulator",
                     "batch",
                     "card in batch",
-                    "imprint",
-                    "absolute card index",
+                    "imprint"
                 ]
             )
             if print_phantoms:
